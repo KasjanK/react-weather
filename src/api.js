2340adfd4b2a9bd9d8a56b32b52f5fdb94ef64d5
KasjanK <70755862+KasjanK@users.noreply.github.com>
2023-12-29 02:51:43 +0100

    Added API fetch for cities & finished city search component

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d8418d11fdmsh468e6c0e1305cc7p1ebfe4jsnaba49c870aa0",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};

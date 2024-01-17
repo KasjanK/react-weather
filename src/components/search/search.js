b4679b2ec037025d6d55453ed0bb354d25d1af8d
KasjanK <70755862+KasjanK@users.noreply.github.com>
2024-01-17 18:05:39 +0100

    changed min population from 300k to 1k

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,8 +7,8 @@ const Search = ({ onSearchChange }) => {
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=300000&namePrefix=${inputValue}`,
-      geoApiOptions
+      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${inputValue}`,
+      geoApiOptions,
     )
       .then((response) => response.json())
       .then((response) => {
